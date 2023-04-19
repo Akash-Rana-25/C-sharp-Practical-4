9c529cdf78fa3bf8d17c5f9cee75ffa31168c6e2
Akash-Rana-25 <akashrana.work.gmail.com>
2023-04-19 14:09:07 +0530

    default statment

--- Practical-4/Practical-4/Student.cs
+++ Practical-4/Practical-4/Student.cs
@@ -51,6 +51,8 @@ namespace Practical_4
                 > 80 => "B",
                 > 70 => "C",
                 <= 70 => "D",
+                //_ => "F" default unreachable. It has already been handled by a previous arm of the switch expression or it is impossible to match.
+
             };
         }
     }
